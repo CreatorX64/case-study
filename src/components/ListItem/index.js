b8b0e7d097f9b8b634e7a6978a0d48232546492a
Hakan Guclu <creatorX64@gmail.com>
2022-05-29 20:28:58 +0300

    refactor

--- src/components/ListItem/index.js
+++ src/components/ListItem/index.js
@@ -13,7 +13,11 @@ const ListItem = ({ item }) => {
   return (
     <StyledListItem>
       <figure>
-        <img src={item.imageUrl} alt={item.name} />
+        {/* loading="lazy" is supported by most popular Chromium-powered
+        browsers (Chrome, Edge, Opera) and Firefox. Even though Safari
+        doesn't support this on iframes, there's support for it on img
+        elements. Source: https://caniuse.com/loading-lazy-attr */}
+        <img src={item.imageUrl} alt={item.name} loading="lazy" />
       </figure>
 
       <section>
